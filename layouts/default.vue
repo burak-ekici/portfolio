64ff2d60fcf5140e7d87a31bca55313f09580476
burak-ekici <78544799+burak-ekici@users.noreply.github.com>
2021-04-14 12:13:38 +0200

    Add files via upload

---
+++ layouts/default.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="fond absolute w-full">
+    <div class="absolute w-full h-full">
+      <Navbar />
+    </div>
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+import Navbar from "@/components/navbar";
+
+export default {
+  components: {
+    Navbar,
+  },
+};
+</script>
+
+<style>
+.fond {
+  background-color: rgb(34, 34, 34);
+  z-index: -50;
+  overflow: hidden;
+}
+</style>
